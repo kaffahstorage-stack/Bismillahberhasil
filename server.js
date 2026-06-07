d7d73628c62453181d7bdc046a390442d6cd2bd6
kaffahstorage-stack <kaffahstorage@gmail.com>
2026-06-08 07:20:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ await db.ref("transactions/" + order_id).set(order);
 });
 
 // ================= START SERVER =================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 SERVER READY ON PORT:", PORT);
