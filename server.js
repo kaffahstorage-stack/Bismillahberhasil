9b1b965a70c4c612e87f7c66177e6c003944268b
kaffahstorage-stack <kaffahstorage@gmail.com>
2026-06-08 14:46:23 +0800

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get("/ping", (req, res) => {
 });
 
 /* ================= START ================= */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const server = app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 SERVER READY ON PORT:", PORT);
